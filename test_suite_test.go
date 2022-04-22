9ddb55670a0e77155b527fcab4be35d2898ea8bd
yulng <wei.yang@daocloud.io>
2022-04-22 14:54:59 +0800

    hello
    hello

--- test_suite_test.go
+++ test_suite_test.go
@@ -1,13 +1,1 @@
-package test_test
-
-import (
-	"testing"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-)
-
-func TestTest(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Test Suite")
-}
+hello
